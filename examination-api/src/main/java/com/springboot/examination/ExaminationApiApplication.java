caa114042af5246c10099801d96f39a1204ee0c2
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-07-08 08:59:38 +0100

    first commit

---
+++ examination-api/src/main/java/com/springboot/examination/ExaminationApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.examination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaminationApiApplication.class, args);
+	}
+
+}
---
+++ examination-api/src/test/java/com/springboot/examination/ExaminationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.examination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
